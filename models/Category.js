6e1cc805d07e7a6c177e805ec6952c300c8dd674
poonambaravkar-node <poonam.baravkar@tekditechnologies.com>
2025-12-18 12:31:49 +0530

    Update Category.js

--- models/Category.js
+++ models/Category.js
@@ -3,6 +3,5 @@ const mongoose = require('mongoose');
 const categorySchema = new mongoose.Schema({
   name: { type: String, required: true }
 });
-// console.log("categorySchema-", categorySchema);
 
 module.exports = mongoose.model('Category', categorySchema);
